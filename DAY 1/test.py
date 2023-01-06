c8c17b038b74d0b3eb3f40dc818bbe9a7a3cac5a
leo <noheunhye1006@gmail.com>
2023-01-06 22:29:21 +0900

    넘나 쉽고 #기본 #쉽다

---
+++ DAY 1/test.py
@@ -0,0 +1,12 @@
+# print("안너ㅓㅓㅓ넝,파ㅏㅏ이ㅣ써ㅓㅓㅓㅓㅓㅓㄴ")
+#j=0 
+#print (j)
+#print(2**100)
+apple=10
+orange=20
+print(orange+apple)
+print(orange-apple)
+print(orange*apple)
+print(orange/apple)
+print(orange//apple)
+print(orange%apple)
