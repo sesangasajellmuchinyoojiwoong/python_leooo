a012d36ddff2b80367ba2fde100cf4b70dfc86a9
leo <noheunhye1006@gmail.com>
2023-01-13 22:10:07 +0900

    52 52 파이썬 믿고 있었다고 52!

---
+++ DAY 2/condition.py
@@ -0,0 +1,46 @@
+'''
+조건문
+<if/else>
+if 조건:
+    참일때 실행할 코드
+else:
+    거짓일때 실행할 코드
+조건:true 또는 false로 답할수 있는 질문
+<for>
+1번
+for 변수 in range (숫자):
+    실행할 코드
+
+2번
+for 변수 in range (시작<이상>,끝<미만>):
+    실행할 코드
+
+3번 변수 in range(시작,끝,증강):
+    실행할 코드    
+
+
+    <중요한 부분>
+  아래 모두 같은 코드임.
+for i in range(5):
+    print("파이썬")
+
+for i in range(0,5):
+    print("파이썬")
+
+for i in range(0,5,1):
+    print("파이썬")    
+'''
+# 오늘날씨 = "비"
+# if 오늘날씨 == "비":
+#    print("토네이도에 빨려가 우산을 타고 날아갔다")
+# else:
+#    print("흐음........어쩔 수 없지.....그냥 맞고 가자 상남자처럼!!!!! 그리고 지웅이는 대머리가 되었다")
+
+# 1번 예시 for i in range(3):
+#    print("서우야 지웅아 온유야")
+
+# 2번 예시 for i in range(1, 11):
+#    print(i)
+
+# 3번 예시for i in range(2, 11, 2):
+#    print(i)
